68b3eb010b4f205b92806731b33b85ae11cf6c36
lakhdar hafsi <lacdart2@hotmail.com>
2024-08-30 16:04:31 +0200

    changed styles,added anumations,fixed overflow bug

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
+
 document.addEventListener("DOMContentLoaded", function () {
+
     const scrollToSection = document.querySelector('a[href="#classes"]');
-
     scrollToSection.addEventListener('click', function (e) {
-        e.preventDefault(); // Prevent the default anchor click behavior
-
+        e.preventDefault();
         const targetSection = document.querySelector("#classes");
         targetSection.scrollIntoView({ behavior: "smooth" });
     });
+
+
+    const cards = document.querySelectorAll('.card');
+    cards.forEach((card, index) => {
+        setTimeout(() => {
+            card.classList.remove('opacity-0', 'translate-x-4');
+            card.classList.add('opacity-100', 'translate-x-0');
+        }, index * 400);
+    });
 });
+
 
 function submitForm() {
     alert('شكرًا على رسالتك. سنقوم بالرد عليك في أقرب وقت!');
-    return true; // Allow the form to be submitted
+    return true;
 }
 
 function sanitizeInput(input) {
